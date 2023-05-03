d6f0008cae05dcf50909441000a73e7b1fe2c392
Himanshu Kashyap <82440705+himanshukhyap@users.noreply.github.com>
2023-05-03 08:12:45 +0530

    d

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<h1>Himanshu</h1>
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome_get.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
