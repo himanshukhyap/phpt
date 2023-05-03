863b3ff204830703bd3282d7f62ad1c3a11fd03f
Himanshu Kashyap <82440705+himanshukhyap@users.noreply.github.com>
2023-05-03 16:24:05 +0530

    cvfd

--- index.php
+++ index.php
@@ -1,12 +1,65 @@
-<!DOCTYPE HTML>
-<html>  
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<form action="welcome_get.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-</body>
-</html>
+    <title>Learning</title>
+  </head>
+  <body>
+ 
+    <?php
+    include 'db_connect.php';
+    ?>
+    
+    <?php
+   // Query the database
+   $sql = "SELECT * FROM MyGuests";
+   $result = $conn->query($sql);
+   
+
+    // Generate HTML code to display the results
+    if ($result->num_rows > 0) {
+        echo "<div class='table-responsive'>";
+        echo "<table class='table'>";
+        echo "<thead class='table-dark'>
+                  <tr>
+                      <th>ID</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Email</th>
+                      <th>Registration Date</th>
+                  </tr>
+              </thead>";
+        echo "<tbody>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["firstname"] . "</td>";
+            echo "<td>" . $row["lastname"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            echo "<td>" . $row["reg_date"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        echo "</div>";
+    } else {
+        echo "No results found.";
+    }
+
+    $conn->close();
+    ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></
